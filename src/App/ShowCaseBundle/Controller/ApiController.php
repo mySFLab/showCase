ceca70931389e706adbdfb67b951d5b65362036a
Laurent BRAU <laurent.brau@gmail.com>
2018-02-03 00:30:46 +0100

    update config.yml, add createMethod, add projectType for validation

--- src/App/ShowCaseBundle/Controller/ApiController.php
+++ src/App/ShowCaseBundle/Controller/ApiController.php
@@ -9,6 +9,7 @@
 namespace App\ShowCaseBundle\Controller;
 
 use App\ShowCaseBundle\Entity\Project;
+use App\ShowCaseBundle\Form\ProjectType;
 use Doctrine\ORM\EntityManager;
 use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\View\View;
@@ -17,6 +18,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ApiController extends Controller
 {
@@ -41,6 +44,37 @@ class ApiController extends Controller
     {
         $projet = $this->getDoctrine()->getRepository(Project::class)->findOneBy(['id' => $id]);
 
+        if (null === $projet) {
+            return new JsonResponse(['message' => 'Project not found'], Response::HTTP_NOT_FOUND);
+        }
+
         return $projet;
+    }
+
+    /**
+     * @Rest\View(statusCode=Response::HTTP_CREATED)
+     * @Rest\Post("/projects/", name="new_project")
+     *
+     * @param Request $request
+     * @return Project|\Symfony\Component\Form\FormInterface
+     */
+    public function addProjectAction(Request $request)
+    {
+
+        $project = new Project();
+
+        $form = $this->createForm(ProjectType::class, $project);
+        $form->submit($request->request->all());
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($project);
+            $em->flush();
+
+            return $project;
+
+        } else {
+            return $form;
+        }
+
     }
 }
---
+++ src/App/ShowCaseBundle/Form/ProjectType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\ShowCaseBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProjectType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('projectName')
+            ->add('picPath')
+            ->add('enabled')
+        ;
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'App\ShowCaseBundle\Entity\Project',
+            'csrf_protection' => false
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'app_showcasebundle_project';
+    }
+}
